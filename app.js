4e6c4dd4fbfe6f3246ab7ee7ee0781c4a8a2ee02
Anderson Sousa <anderson.lima18pb@gmail.com>
2025-03-30 10:40:25 -0300

    tesntando css

--- app.js
+++ app.js
@@ -517,7 +517,7 @@ app.post('/login', async (req, res) => {
     
     res.cookie('token', token, { 
       httpOnly: true, 
-      secure: false, 
+      secure: true, 
       maxAge: 60 * 60 * 1000 
     });
 
